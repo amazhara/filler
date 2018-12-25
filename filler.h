b3a24073340fb30b6203049c674674a858bc3c3f
Artur MAZHARA <amazhara@e2r11p17.unit.ua>
2018-12-25 19:50:08 +0200

    go

---
+++ filler.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   filler.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amazhara <amazhara@student.unit.ua>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/12/25 17:29:19 by amazhara          #+#    #+#             */
+/*   Updated: 2018/12/25 17:42:01 by amazhara         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef _FILLER_H
+# define _FILLER_H
+#include <stdlib.h>
+#include <unistd.h>
+#include "printf/libft/libft.h"
+#include "printf/ft_printf.h"
+
+#endif
